acbfdaa8abba4d51981e528b4e2d49b146c893b2
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-06-13 02:22:00 +0530

    Edabit-String-Loop-Question

---
+++ Edabit-str-Loop-Qeustion.js
@@ -0,0 +1,62 @@
+// 16}
+
+// Create a function that takes a word and returns the new word without including the first character.
+// Examples
+// newWord("apple") ➞ "pple"
+// newWord("cherry") ➞ "herry"
+// newWord("plum") ➞ "lum"
+
+function newWord(str) {
+    let main = ""
+    for (let i = 1; i < str.length; i++) {
+        main += str[i]
+    }
+    return main
+}
+console.log(newWord("apple"))
+console.log(newWord("cherry"))
+console.log(newWord("plum"))
+
+// 23}
+
+// Create a function that takes as a parameter an array of "stringified" numbers and returns an array of numbers.
+// Example:
+// ["1", "3", "3.6"] ➞ [1, 3, 3.6]
+// Examples
+// toNumberArray(["9.4", "4.2"]) ➞ [9.4, 4.2]
+// toNumberArray(["91", "44"]) ➞ [91, 44]
+// toNumberArray(["9.5", "8.8"]) ➞ [9.5, 8.8]
+
+function toNumberArray(arrstrnum) {
+    let main = [];
+    for (let i = 0; i < arrstrnum.length; i++) {
+        main.push(arrstrnum[i] * 1)
+    }
+    return main
+}
+
+console.log(toNumberArray(["9.4", "4.2"]));
+console.log(toNumberArray(["91", "44"]));
+console.log(toNumberArray(["9.5", "8.8"]));
+
+
+// 24}
+
+// Create a function that takes a string; we'll say that the front is the first three characters of the string. If the string length is less than three characters, the front is whatever is there. Return a new string, which is three copies of the front.
+// Examples
+// frontThree("Python") ➞ "PytPytPyt"
+// frontThree("Cucumber") ➞ "CucCucCuc"
+// frontThree("bioshock") ➞ "biobiobio"
+
+function frontThree(str) {
+    let main = "";
+    let stratend = str.slice(0, 3)
+    for (let i = 0; i < 3; i++) {
+        main += stratend
+
+    }
+    return main
+}
+console.log(frontThree("Python"));
+console.log(frontThree("Cucumber"));
+console.log(frontThree("bioshock")); 
